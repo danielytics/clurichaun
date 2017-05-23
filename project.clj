d0942ae8b7e807ea0cbe9f163cf87cb4d4a849db
danielytics <danielytics@users.noreply.github.com>
2017-05-23 20:20:44 +0100

    add GitHub URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clurichaun "0.1.0-SNAPSHOT"
   :description "Clurichaun is a WebGL game engine"
-  :url ""
+  :url "https://github.com/danielytics/clurichaun"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
